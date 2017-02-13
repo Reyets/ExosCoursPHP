0d8165156db338a0c80fef53a73f4b5596e75bef
Antoine STEYER <antoine.steyer@etu.unice.fr>
2017-02-13 17:19:28 +0100

    ajout variables.php

---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+
+$ageDuVisiteur = 20;
+echo 'Le visiteur a ' . $ageDuVisiteur . ' ans .';
+
+$prix = 2.5;
+$quantite = 10;
+$total = $prix * $quantite;
+
+echo 'Cela coute ' . $total . ' euros.';
+
+?>
