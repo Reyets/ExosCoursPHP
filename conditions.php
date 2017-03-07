0bfbdf95e0759cbed4c1b13ad8bbed769fa80cd3
Antoine STEYER <antoine.steyer@etu.unice.fr>
2017-03-07 15:32:53 +0100

    fichier conditions

---
+++ conditions.php
@@ -0,0 +1,17 @@
+<?php
+$age = 8;
+
+if ($age <= 12)
+{
+    echo "Salut gamin ! Bienvenue sur mon site !<br />";
+    $autorisation_entrer = "Oui";
+} 
+else
+{
+    echo "Ceci est un site pour enfants, vous êtes trop vieux pour pouvoir entrer. Au revoir !<br />";
+    $autorisation_entrer = "Non";
+}
+
+echo "Avez-vous l'autorisation d'entrer ? La réponse est : $autorisation_entrer";
+
+?>
